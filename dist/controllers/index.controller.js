647c3bb2e18449f8b17c7d44ba90f9dd8e8c3565
pedroresd1 <60554958+pedroresd1@users.noreply.github.com>
2020-02-11 13:27:42 -0300

    first commit
    teste

---
+++ dist/controllers/index.controller.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function indexWelcome(req, res) {
+    return res.json('Welcome to my API');
+}
+exports.indexWelcome = indexWelcome;
+;
